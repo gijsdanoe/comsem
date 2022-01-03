d79931310df147700ea61f59ca1a9b6c883c1998
miaoyinchan <miaoyinchan@hotmail.com>
2022-01-03 14:28:02 +0100

    change zero to O

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import json
+import utils
+
 from sklearn.metrics import (
     accuracy_score,
     confusion_matrix,
@@ -6,14 +9,25 @@ from sklearn.metrics import (
 )
 
 OUTPUT_DIR = "Output/"
+DATA_DIR = "Data/"
 
 
-def get_gold_label(file):
+def get_labels(file):
+    doc_dict = utils.read_file(file)
+    for i in doc_dict:
+        for x in doc_dict[i]:
+            for y in doc_dict[i][x]:
+                print(y)
 
+    # return file
+
+
+def get_predicted_label(file):
+    pass
 
 
 def evaluate(Y_test, Y_pred):
-    # save the confusion matrix of the model in png file
+    # Save the confusion matrix of the model in png file
     cm = confusion_matrix(Y_test, Y_pred)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
     disp.plot()
@@ -24,4 +38,11 @@ def evaluate(Y_test, Y_pred):
     print("Final accuracy: {}".format(acc))
 
 
+def main():
+    file = DATA_DIR + "tokens.json"
+    get_labels(file)
 
+
+
+if __name__ == '__main__':
+    main()
