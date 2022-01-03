4ef8c132b63a054203959c14ad973d96c0804f4b
miaoyinchan <miaoyinchan@hotmail.com>
2022-01-03 14:58:53 +0100

    update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -2,24 +2,19 @@ import pandas as pd
 import json
 import utils
 
-from sklearn.metrics import (
-    accuracy_score,
-    confusion_matrix,
-    ConfusionMatrixDisplay,
-)
 
 OUTPUT_DIR = "Output/"
 DATA_DIR = "Data/"
 
 
-def get_labels(file):
+def get_labels(file, n):
+    labels = []
     doc_dict = utils.read_file(file)
     for i in doc_dict:
         for x in doc_dict[i]:
-            for y in doc_dict[i][x]:
-                print(y)
+            labels.append(doc_dict[i][x][n])
 
-    # return file
+    return labels
 
 
 def get_predicted_label(file):
@@ -27,21 +22,24 @@ def get_predicted_label(file):
 
 
 def evaluate(Y_test, Y_pred):
-    # Save the confusion matrix of the model in png file
-    cm = confusion_matrix(Y_test, Y_pred)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
-    disp.plot()
-    plt.savefig(OUTPUT_DIR + "{}.png".format(experiment_name))
 
-    # Obtain the accuracy score of the model
-    acc = accuracy_score(Y_test, Y_pred)
-    print("Final accuracy: {}".format(acc))
+    total_count = len(Y_test)
+    correct_pred = 0
+
+    for g, p in zip(Y_test, Y_pred):
+        if g == p:
+            correct_pred = correct_pred + 1
+
+    acc = round((correct_pred / total_count * 100), 2)
+
+    return acc
 
 
 def main():
     file = DATA_DIR + "tokens.json"
-    get_labels(file)
-
+    Y_test = get_labels(file, 0)
+    Y_pred_baseline = get_labels(file, 1)
+    print(f'baseline accuracy: {evaluate(Y_test, Y_pred_baseline)}')
 
 
 if __name__ == '__main__':
